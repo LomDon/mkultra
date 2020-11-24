44f6d968757eca5e18b95d7c041cf0473bd402fc
Andrey <osum2@mail.ru>
2020-11-24 13:04:00 +0300

    0.0.5 - финал

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import './App.css';
 import {Menu} from "./components/Menu";
-import {Route,BrowserRouter} from "react-router-dom";
+import  {Route,BrowserRouter} from "react-router-dom";
 import {Pages} from "./pages/Pages";
-import {AddPage} from "./pages/addPage";
+import {AddPage} from "./pages/AddPage";
+import {EditPage} from "./pages/EditPage";
+import {Branches} from "./pages/Branches";
+import {AddBranch} from "./pages/AddBranch";
+import {cmsPath} from "./config";
 
 function App() {
-  return (<div className="container-fluid">
-        <BrowserRouter>
-          <div className="row">
-            <Menu/>
-            <div className="col-9">
-              <Route exact path="/" render={()=>"CMS"}/>
-              <Route exact path="/pages/" render={()=><Pages/>}/>
-              <Route path="/pages/addPage" render={()=><AddPage/>}/>
-                <Route path="/pages/editPage" render={()=><AddPage/>}/>
+  return (
+      <div className="container-fluid">
+          <BrowserRouter>
+            <div className="row">
+                <Menu/>
+                <div className="col-9">
+                    <Route exact path={cmsPath+"/"} render={()=>"CMS"}/>
+                    <Route exact path={cmsPath+"/pages/"} render={()=><Pages/>}/>
+                    <Route path={cmsPath+"/pages/addPage"} render={()=><AddPage/>}/>
+                    <Route path={cmsPath+"/pages/editPage"} render={()=> <EditPage/>}/>
+                    <Route exact path={cmsPath+"/branches/"} render={()=><Branches/>}/>
+                    <Route path={cmsPath+"/branches/addBranch"} render={()=><AddBranch/>}/>
+                </div>
             </div>
-          </div>
-        </BrowserRouter>
+          </BrowserRouter>
       </div>
 
   );
